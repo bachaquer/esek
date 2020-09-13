38e38405f2df04ed14fa66f9ff5490864c257d7b
Batyrkhan <aytp84@gmail.com>
2020-09-13 17:54:33 +0500

    eh

--- javasik/js/scriptik.js
+++ javasik/js/scriptik.js
@@ -1,1 +1,12 @@
-var a = "hello"
+function bake(sake, mult){
+	return sake*mult;
+}
+
+function multik(num){
+	var wow = function tamam(x){
+		return bake(x, num); 
+	};
+	return wow;
+}
+var dvadcatka=multik(20);
+console.log(dvadcatka(5));	
