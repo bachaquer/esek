8c3752124ce02eb7295409617e84b0b43b2bc39f
Batyrkhan <aytp84@gmail.com>
2020-09-19 01:21:59 +0500

    uff

--- javasik/js/scriptik.js
+++ javasik/js/scriptik.js
@@ -1,12 +1,35 @@
-function bake(sake, mult){
-	return sake*mult;
-}
+document.addEventListener("DOMContentLoaded",
+	function(event){
+		var title = document.querySelector("#title").textContent;
+		function saysalam(event) {
+			console.log(event);
+			this.textContent="madis";
+			var name = document.querySelector("#name").value;
+			console.log(name);
+			var message = "<h2>Salamaleikum " + name + "!</h2>";
 
-function multik(num){
-	var wow = function tamam(x){
-		return bake(x, num); 
-	};
-	return wow;
-}
-var dvadcatka=multik(20);
-console.log(dvadcatka(5));	
+			document.querySelector("#content").innerHTML = message;
+
+			if(name === "Adil" || name === "Madina" || name === "Muslim"){
+				document.querySelector("#title").textContent = title + " & vashi lubimye chebureki!";
+			}
+			else{
+				document.querySelector("#title").textContent = title;
+			}
+		}
+
+		document.querySelector("button")
+			.addEventListener("click", saysalam);
+
+		document.querySelector("body")
+			.addEventListener("mousemove",
+				function (event){
+					if (event.shiftKey === true) {
+						console.log("x : " + event.clientX);
+						console.log("y : " + event.clientY);
+					}
+				}
+			);
+
+	}
+);
